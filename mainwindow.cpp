dd7711123d8c88f918e38fcf19d156b096755c16
mouaiadsalaas <mouaiadsalaas@gmail.com>
2020-12-11 16:40:33 +0300

    first commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTimer>
+#include <QDebug>
+#include <QTextStream>
+#include <QTime>
+#include <QDate>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QTimer *mTimer = new QTimer(this);
 /**********************/
 /*****third step******/
 /********************/
     //here we setup our library and set our pins mode then go to our slot functions
     wiringPiSetup();
     pinMode(5,OUTPUT);
+
+    fd = wiringPiSPISetup(CHANNEL, 500000);
+    fd_2 = wiringPiSPISetup(CHANNEL_2, 500000);
+
+    connect(mTimer, SIGNAL(timeout()), this, SLOT(updateCaption()));
+    mTimer->start(500);
+
 }
 
 MainWindow::~MainWindow()
@@ -21,10 +36,42 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_ON_clicked()
 {
+    // our tx data is buffer[0]
+    TX_buffer[0] = 0xF0;
+    wiringPiSPIDataRW(CHANNEL_2, TX_buffer, 1);//send the buffer //our channel ,our data,how much bit
+
     digitalWrite(5,HIGH);
+
 }
 
 void MainWindow::on_OFF_clicked()
 {
+    // our tx data is buffer[0]
+
+
     digitalWrite(5,LOW);
+    //sleep(5);
+}
+
+//function has been created for timer duty
+void MainWindow::updateCaption()
+{
+    //RX_buffer[0] = 0x00;
+    //coming data from spi bus is being written over the  same buffer (RX_buffer)
+    wiringPiSPIDataRW(CHANNEL_2,RX_buffer, 2);
+    timerCount++;
+    qDebug() << "Timer = " << timerCount << " geln = "<< RX_buffer;
+}
+
+
+//this function has been created for showing time by using QTIME lib
+void MainWindow::on_ShowTime_clicked()
+{
+    QTextStream out(stdout);
+
+    QDate cd = QDate::currentDate();
+    QTime ct = QTime::currentTime();
+
+    qDebug() << "Current date is: " << cd.toString();
+    qDebug()<< "Current time is: " << ct.toString();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,9 @@
 //here we include our library
 //then we go to ui and creat our buttons and slots
 #include "wiringPi.h"
+#include "wiringPiSPI.h"
+#include <unistd.h>
+
 
 namespace Ui {
 class MainWindow;
@@ -26,8 +29,21 @@ private slots:
 
     void on_OFF_clicked();
 
+    void updateCaption();
+
+    void on_ShowTime_clicked();
+
 private:
     Ui::MainWindow *ui;
+    int fd,fd_2, result;
+    int rcv=0;
+    unsigned char TX_buffer[100];
+    unsigned char RX_buffer[100];
+    static const int CHANNEL = 1;
+    static const int CHANNEL_2 = 0;
+
+    int timerCount =0;
+
 };
 
 #endif // MAINWINDOW_H
